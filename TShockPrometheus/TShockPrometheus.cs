d5f747485f88962620f0f7667e05dc8c6d09d750
Svante Bengtson <svante.b37@gmail.com>
2021-01-03 16:14:26 +0100

    add player counter

--- TShockPrometheus/TShockPrometheus.cs
+++ TShockPrometheus/TShockPrometheus.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
+using Prometheus;
 
 namespace TShockPrometheus {
   [ApiVersion(2, 1)]
@@ -11,6 +12,9 @@ namespace TShockPrometheus {
     public override string Author { get { return "Swantzter"; } }
     public override string Description { get { return "Exports TShock data to Prometheus"; } }
 
+    private static readonly Gauge ConnectedPlayersGauge = Metrics.CreateGauge("tshock_connected_player_count", "connected players");
+    private static readonly MetricServer server = new MetricServer(hostname: "localhost", port: 9763);
+
     /// <summary>
     /// The plugin's constructor
     /// Set your plugin's order (optional) and any other constructor logic here
@@ -19,25 +23,34 @@ namespace TShockPrometheus {
 
     }
 
-    /// <summary>
-    /// Performs plugin initialization logic.
-    /// Add your hooks, config file read/writes, etc here
-    /// </summary>
+    #region Initialize/Dispose
     public override void Initialize() {
-      throw new NotImplementedException();
+      server.Start();
+
+      ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+      ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
     }
 
-    /// <summary>
-    /// Performs plugin cleanup logic
-    /// Remove your hooks and perform general cleanup here
-    /// </summary>
     protected override void Dispose(bool disposing) {
       if (disposing) {
-        //unhook
-        //dispose child objects
-        //set large objects to null
+        server.Stop();
+
+        ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+        ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
       }
       base.Dispose(disposing);
     }
+    #endregion
+
+    #region Hooks
+    private void OnLeave(LeaveEventArgs args) {
+      if (args.Who == TShock.Players.Count(p => p != null)) return;
+      ConnectedPlayersGauge.Dec(1);
+    }
+
+    private void OnJoin(JoinEventArgs args) {
+      ConnectedPlayersGauge.Inc(1);
+    }
+    #endregion
   }
 }
