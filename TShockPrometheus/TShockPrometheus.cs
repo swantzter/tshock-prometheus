b8a58438169add45dfc17994f19e0cee36adc1e2
Svante Bengtson <svante.b37@gmail.com>
2021-01-03 15:33:24 +0100

    scaffold project

---
+++ TShockPrometheus/TShockPrometheus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace TShockPrometheus {
+  [ApiVersion(2, 1)]
+  public class TShockPrometheus : TerrariaPlugin {
+    public override string Name { get { return "TShockPrometheus"; } }
+    public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+    public override string Author { get { return "Swantzter"; } }
+    public override string Description { get { return "Exports TShock data to Prometheus"; } }
+
+    /// <summary>
+    /// The plugin's constructor
+    /// Set your plugin's order (optional) and any other constructor logic here
+    /// </summary>
+    public TShockPrometheus(Main game) : base(game) {
+
+    }
+
+    /// <summary>
+    /// Performs plugin initialization logic.
+    /// Add your hooks, config file read/writes, etc here
+    /// </summary>
+    public override void Initialize() {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Performs plugin cleanup logic
+    /// Remove your hooks and perform general cleanup here
+    /// </summary>
+    protected override void Dispose(bool disposing) {
+      if (disposing) {
+        //unhook
+        //dispose child objects
+        //set large objects to null
+      }
+      base.Dispose(disposing);
+    }
+  }
+}
